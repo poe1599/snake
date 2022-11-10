fbbb64499d23617ab7319a56f260f6d3fbbb09a4
Allen Yeh <poe1599@gmail.com>
2022-11-10 17:06:33 +0800

    deploy

---
+++ precache-manifest.7b04ba01ba5ca346cef4a9fa56a9337f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65a67be73f04c5c54603",
+    "url": "/snake/css/app.0295ffcf.css"
+  },
+  {
+    "revision": "6485a3fbda77d85cfc91833299745dce",
+    "url": "/snake/img/space.6485a3fb.jpg"
+  },
+  {
+    "revision": "b99ca1b613c0498420d93b01eb4fdcda",
+    "url": "/snake/index.html"
+  },
+  {
+    "revision": "7091dde9381c7695a507",
+    "url": "/snake/js/about.715b23f4.js"
+  },
+  {
+    "revision": "65a67be73f04c5c54603",
+    "url": "/snake/js/app.d1c27d58.js"
+  },
+  {
+    "revision": "b5e56e465d953e19f14f",
+    "url": "/snake/js/chunk-vendors.1e703c72.js"
+  },
+  {
+    "revision": "43c4cbc9269bcb912e386d2c7ef4266a",
+    "url": "/snake/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/snake/robots.txt"
+  }
+]);
